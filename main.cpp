fe2d59b21941acc21a0c337a678b6fdfced5e9b4
Sylvia Crowe <software@oneirocosm.com>
2025-08-31 21:14:13 -0700

    line drawing third optimization

--- main.cpp
+++ main.cpp
@@ -17,19 +17,18 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
         std::swap(ax, ay);
         std::swap(bx, by);
     }
-    float slope = std::abs(by - ay)/static_cast<float>(bx - ax);
     int y = ay;
-    float error = 0;
+    int ierror = 0;
     for (int x = ax; x <= bx; x++) {
         if (transpose) {
             framebuffer.set(y, x, color);
         } else {
             framebuffer.set(x, y, color);
         }
-        error += slope;
-        if (error >= 0.5) {
+        ierror += 2 * std::abs(by - ay);
+        if (ierror > bx - ax) {
             y += by > ay ? 1 : -1;
-            error -= 1;
+            ierror -= 2 * (bx - ax);
         }
     }
 }
